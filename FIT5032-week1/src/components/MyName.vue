1e9dcce21553e8d154ab61186f67e3ba1b3ecef2
yxcmongnan <1003314442@qq.com>
2024-08-02 14:43:28 +1000

    MyName.Vue

---
+++ FIT5032-week1/src/components/MyName.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      Welcome: {{ name }}
+      <br />
+      <a href="https://github.com/yxcmongnan" target="_blank" rel="noopener">Xicheng Ye's GitHub</a>
+    </div>
+  </template>
+  
+  <script setup>
+  const name = 'Xicheng Ye'; 
+  </script>
+  
+  <style scoped>
+  div {
+    font-size: 20px;
+    color: #42b983; 
+  }
+  
+  a {
+    color: #007bff;
+    text-decoration: none;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
